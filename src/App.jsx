3754e8135a1a1b409cb2ad88549591a112cf13c3
MuntasirMahmud <muntasirm525@gmail.com>
2025-09-18 17:13:10 +0600

    initial project set up with react router done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import { Outlet } from "react-router";
 function App() {
   return (
     <div className="">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }

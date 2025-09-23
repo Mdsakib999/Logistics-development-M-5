238d30b08d8819a4b6d979a5e59efa2a2119cc83
MuntasirMahmud <muntasirm525@gmail.com>
2025-09-23 15:33:50 +0600

    responsive issues fixed

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ children, to, type = "primary" }) {
   return (
     <Link
       to={to}
-      className={`flex items-center text-base justify-center w-[10.5rem] h-[3.25rem] rounded-md py-5 px-10 ${
+      className={`flex items-center text-base justify-center w-[10.5rem] h-[3.25rem] rounded-md py-5 px-8 ${
         type === "primary"
           ? "bg-primary text-white hover:bg-white hover:border-2 hover:border-black hover:text-black"
           : type === "secondary"

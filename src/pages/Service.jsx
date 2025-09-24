7a87f96a116620b99ed178645429abfe681a2065
MuntasirMahmud <muntasirm525@gmail.com>
2025-09-24 15:30:34 +0600

    faq section ui and responsiveness completed

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,3 +1,5 @@
+import Faq from "../components/sections/Faq";
+import HowItWorks from "../components/sections/HowItWorks";
 import Solution from "../components/sections/Solution";
 import Banner from "../components/service/Banner";
 import Offer from "../components/service/Offer";
@@ -8,6 +10,8 @@ export default function Service() {
       <Banner />
       <Solution />
       <Offer />
+      <HowItWorks />
+      <Faq />
     </div>
   );
 }
